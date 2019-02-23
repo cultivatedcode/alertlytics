9ec35b25cc697439da4a36f2ecfdd81a7f80f044
CJ Baker <cj.baker@cultivatedcode.com>
2019-02-23 08:17:30 -0800

    Alerts on a timer.

--- lib/alertlytics/workers/alert.ex
+++ lib/alertlytics/workers/alert.ex
@@ -16,45 +16,78 @@ defmodule Alertlytics.Workers.Alert do
   end
 
   @doc """
-    Optionally send an alert based on whether the previous_is_live is different from the new_is_live.
+    Optionally queue an alert based on whether the previous_is_live is different from the new_is_live.
   """
-  def optionally_send_alert(service_config, previous_is_live, new_is_live) do
-    GenServer.cast(__MODULE__, {:send_alert, service_config, previous_is_live, new_is_live})
+  def update_alert(service_config, previous_is_live, new_is_live) do
+    GenServer.cast(__MODULE__, {:add_alert, service_config, previous_is_live, new_is_live})
   end
 
   # Server (Callbacks)
 
   def init(init_args) do
+    IO.puts(Enum.count(init_args))
+    schedule_work()
     {:ok, init_args}
   end
 
-  def handle_cast({:send_alert, service_config, previous_is_live, new_is_live}, _state) do
-    message_sent =
-      if previous_is_live != new_is_live && previous_is_live != nil do
-        IO.puts("ALERTING")
-        channel = Alertlytics.Workers.Config.channel()
+  def handle_info(:work, state) do
+    attachments =
+      Enum.map(state, fn service_detail ->
+        string_state =
+          if service_detail[:new_is_live] do
+            "fixed"
+          else
+            "failed"
+          end
 
-        if new_is_live do
-          Slack.Web.Chat.post_message(channel, "testing", %{
-            attachments:
-              "[{'color': '#36a64f', 'title': '`#{service_config["name"]}` health check fixed', 'ts': #{
-                DateTime.to_unix(DateTime.utc_now())
-              }}]"
-          })
+        color =
+          if service_detail[:new_is_live] do
+            "#36a64f"
+          else
+            "#f90c0c"
+          end
 
-          true
-        else
-          Slack.Web.Chat.post_message(channel, "testing", %{
-            attachments:
-              "[{'color': '#f90c0c', 'title': '`#{service_config["name"]}` health check failed', 'ts': #{
-                DateTime.to_unix(DateTime.utc_now())
-              }}]"
-          })
+        title = "`#{service_detail[:service_config]["name"]}` health check #{string_state}"
 
-          false
-        end
-      end
+        attachment = %{
+          color: color,
+          title: title,
+          ns: DateTime.to_unix(DateTime.utc_now())
+        }
 
-    {:noreply, message_sent}
+        attachment
+      end)
+
+    if Enum.count(attachments) > 0 do
+      channel = Alertlytics.Workers.Config.channel()
+      Alertlytics.Services.SlackService.post_message("Service Status Update", attachments, channel)
+    end
+
+    schedule_work()
+
+    {:noreply, []}
+  end
+
+  def handle_info(_, state) do
+    {:ok, state}
+  end
+
+  defp schedule_work do
+    one_minute = 60_000
+    Process.send_after(self(), :work, one_minute)
+  end
+
+  def handle_cast({:add_alert, service_config, previous_is_live, new_is_live}, state) do
+    if previous_is_live != new_is_live && previous_is_live != nil do
+      service_state = %{
+        service_config: service_config,
+        new_is_live: new_is_live,
+        previous_is_live: previous_is_live
+      }
+
+      {:noreply, [service_state | state]}
+    else
+      {:noreply, state}
+    end
   end
 end
