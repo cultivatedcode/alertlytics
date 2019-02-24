775ff4f3a0cae51d76fde946f7785e874563991a
CJ Baker <cj.baker@cultivatedcode.com>
2019-02-24 07:33:10 -0800

    Format fixing

--- test/alertlytics/alertlytics_test.exs
+++ test/alertlytics/alertlytics_test.exs
@@ -8,22 +8,22 @@ defmodule AlertlyticsTest do
   test "config service is available" do
     with_mock Slack.Bot,
       start_link: fn _slack, _state, _token -> {:ok, self()} end do
+      {:ok, _pid} = Subject.start(nil, nil)
 
-        {:ok, _pid} = Subject.start(nil, nil)
-        assert [
-             %{
-               "health_check_url" => "https://www.cultivatedcode.com",
-               "name" => "marketing-site",
-               "type" => "web",
-               "test_interval_in_minutes" => 5
-             },
-             %{
-               "health_check_url" => "http://web",
-               "name" => "other",
-               "test_interval_in_minutes" => 1,
-               "type" => "web"
-             }
-           ] == Alertlytics.Workers.Config.services()
+      assert [
+               %{
+                 "health_check_url" => "https://www.cultivatedcode.com",
+                 "name" => "marketing-site",
+                 "type" => "web",
+                 "test_interval_in_minutes" => 5
+               },
+               %{
+                 "health_check_url" => "http://web",
+                 "name" => "other",
+                 "test_interval_in_minutes" => 1,
+                 "type" => "web"
+               }
+             ] == Alertlytics.Workers.Config.services()
     end
   end
 end
