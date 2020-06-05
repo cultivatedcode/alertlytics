f32e0191c31f033450c1b2ea2faa898705a7616c
CJ Baker <cj.baker@cultivatedcode.com>
2020-06-05 06:45:03 -0700

    Simple connection.

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -1,7 +1,14 @@
 defmodule HelloWeb.PageController do
   use HelloWeb, :controller
+  require Logger
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    services = Alertlytics.Workers.Config.services()
+    Logger.info("-Init #{Enum.count(services)}")
+
+    conn
+    |> Plug.Conn.assign(:status, "Operational")
+    |> Plug.Conn.assign(:services, services)
+    |> render("index.html")
   end
 end
